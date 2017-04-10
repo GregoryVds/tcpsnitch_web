09ea4c403675f8faddfb28172b6e36137fd422bf
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2017-04-10 15:52:31 +0200

    Controller also use https urls in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def default_url_options(options={})
+    { protocol: (Rails.env.production? ? :https : :http) }
+  end
 end
