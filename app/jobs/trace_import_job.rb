907ed95febb8fc04835e259f83c758cb99370732
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2017-04-10 17:25:15 +0200

    Correct typo

--- app/jobs/trace_import_job.rb
+++ app/jobs/trace_import_job.rb
@@ -22,7 +22,7 @@ class TraceImportJob < ActiveJob::Base
   end
 
   def extract_archive
-    @extract_dir = `mktemp -p ~/tmp -d`.chomp("\n")
+    @extract_dir = `mktemp -p ./tmp -d`.chomp("\n")
     extract_cmd = @app_trace.archive_is_zip ? "unzip" : "tar -xzf"
     system("cd #{@extract_dir} && #{extract_cmd} #{@app_trace.archive.file.path}")
   end
