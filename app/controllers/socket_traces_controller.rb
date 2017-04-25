88231b498c85d5ed74c2bbe9b5cc7f32436dbc55
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2017-04-25 18:17:33 +0200

    Add temporary fake-grid-item class & fix bug in socket traces controller

--- app/controllers/socket_traces_controller.rb
+++ app/controllers/socket_traces_controller.rb
@@ -1,7 +1,7 @@
 class SocketTracesController < ApplicationController
   def show
     @socket_trace = SocketTrace.find(params[:id])
-    @stat_categories = StatCategory.left_menu(@socket_trace.analysable_type)
+    @analysis = @socket_trace.analysis
     @events = Event.only(:details, :errno, :return_value, :success,
                          :timestamp_usec, :thread_id, :type)
                    .where(socket_trace_id: params[:id])
